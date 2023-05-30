620b5e4d604959542c8743e8f19ed9cf7db95270
mikigo <huangmingqiang@uniontech.com>
2023-05-30 13:08:23 +0800

    update

---
+++ pdocr_rpc/__version__.py
@@ -0,0 +1,2 @@
+__title__ = "pdocr_rpc"
+__version__ = "1.1.3"
--- test/test_ocr.py
+++ test/test_ocr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import pytest
 
-from pdocr_rpc.ocr import ocr
+from pdocr_rpc.ocr import OCR
 
 from pdocr_rpc.setting import setting
 
@@ -9,7 +9,7 @@ setting.SERVER_IP = "10.8.13.78"
 setting.PORT = "8890"
 
 def test_ocr():
-    res = ocr()
+    res = OCR.ocr()
     assert res is not None
 
 if __name__ == '__main__':
