3a3a923508bb195c0c342c1543ede517949a7490
mikigo <huangmingqiang@uniontech.com>
2023-05-31 17:04:50 +0800

    update

--- publish.sh
+++ publish.sh
@@ -5,5 +5,8 @@ rm -rf ./site
 rm -rf ./dist
 rm -rf ./pdocr_rpc.egg-info
 
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade build
+python3 -m pip install --upgrade twine
 python3 -m build
 twine upload dist/*
